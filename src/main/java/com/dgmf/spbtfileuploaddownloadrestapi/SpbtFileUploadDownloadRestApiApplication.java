328145ee5ee52c746c667f4d677e0aa01f2669b5
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-14 09:21:53 +0200

    Project initialization

---
+++ src/main/java/com/dgmf/spbtfileuploaddownloadrestapi/SpbtFileUploadDownloadRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.dgmf.spbtfileuploaddownloadrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbtFileUploadDownloadRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbtFileUploadDownloadRestApiApplication.class, args);
+	}
+
+}
